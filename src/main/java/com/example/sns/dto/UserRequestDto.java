8555159fd663d99b73266b414d1f196382f8716f
whdwon <whdwon09@gmail.com>
2026-05-17 13:12:35 +0900

    style: UserRequestDto 유효성 검사 메시지 추가

--- src/main/java/com/example/sns/dto/UserRequestDto.java
+++ src/main/java/com/example/sns/dto/UserRequestDto.java
@@ -5,10 +5,11 @@ import jakarta.validation.constraints.NotBlank;
 
 // 유저를 등록할 때 필요한 정보
 public record UserRequestDto(
-        @NotBlank
+        @NotBlank(message = "이름은 필수입니다.")
         String username,
-        @NotBlank @Email
+        @NotBlank(message = "이메일은 필수입니다.")
+        @Email(message = "이메일 형식이 올바르지 않습니다.")
         String email,
-        @NotBlank
+        @NotBlank(message = "비밀번호는 필수입니다.")
         String password
 ) {}
